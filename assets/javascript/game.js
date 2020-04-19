782c5250b68d3f38c5af33cc54272253bcf3a91a
sierra <haleyalton93@gmail.com>
2020-04-18 19:54:39 -0700

    assets/javascript/game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,26 @@
+notes
+{/* <audio controls id="music">
+<source src="mj.mp3" type="audio/mpeg">
+Your browser does not support the audio element.
+</audio>
 
+<p>Click the buttons to play or pause the music.</p>
+
+<button onclick="play()" type="button">Play </button>
+<button onclick="pause()" type="button">Pause</button>
+
+<script>
+var myMusic= document.getElementById("music");
+function play() {
+myMusic.play();
+}
+
+function pause() {
+myMusic.pause();
+}
+</script> */}
+
+// function play() {
+//     var audio = new Audio();
+//     audio.play();
+// }
