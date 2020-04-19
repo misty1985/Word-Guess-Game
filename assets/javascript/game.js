de67dbfb1d84e686e1abe8d2a3169e8e5403623a
sierra <haleyalton93@gmail.com>
2020-04-18 23:00:45 -0700

    words and stuffs.

---
+++ assets/javascript/game.js
@@ -0,0 +1,90 @@
+
+var selectableWords =
+    [
+“Bulbasaur”, “Ivysaur”, “Venusaur”, “Charmander”, “Charmeleon”, “Charizard”, “Squirtle”, “Wartortle”, “Blastoise”, “Caterpie”, “Metapod”, “Butterfree”, “Weedle”, “Kakuna”, “Beedrill”, “Pidgey”, “Pidgeotto”, “Pidgeot”, “Rattata”, “Raticate”, “Spearow”, “Fearow”, “Ekans”, “Arbok”, “Pikachu”, “Raichu”, “Sandshrew”, “Sandslash”, “Nidoran”, “Nidorina”, “Nidoqueen”, “Nidorino”, “Nidoking”, ”Clefairy”, “Clefable”, “Vulpix”, “Ninetales”, “Jigglypuff”, “Wigglytuff”, “Zubat”, “Golbat”, “Oddish”, “Gloom”, “Vileplume”, ”Paras”, “Parasect”, “Venonat”, “Venomoth”, “Diglett”, “Dugtrio”, “Meowth”, ”Persian”, “Psyduck”, “Golduck”, “Mankey”, “Primeape”, “Growlithe”, “Arcanine”, “Poliwag”, “Poliwhirl”, “Poliwrath”, “Abra”, “Kadabra”, “Alakazam”, “Machop”, “Machoke”, “Machamp”, “Bellsprout”, “Weepinbell”, “Victreebel”, “Tentacool”, “Tentacruel”, “Geodude”, “Graveler”, “Golem”, ”Ponyta”, “Rapidash”, “Slowpoke”, “Slowbro”, “Magnemite”, “Magneton”, “Farfetchd”, “Doduo”, “Dodrio”.“Seel”, “Dewgong”, “Grimer”, “Muk”, “Shellder”, “Cloyster”, “Gastly”, “Haunter”, “Gengar”, “Onix”, “Drowzee”, “Hypno”, “Krabby”, “Kingler”, “Voltorb”, “Electrode”, “Exeggcute”, “Exeggutor”, “Cubone”, “Marowak”, “Hitmonlee”, “Hitmonchan”, “Lickitung”, “Koffing”, “Weezing”, “Rhyhorn”, “Rhydon”, “Chansey”, “Tangela”, “Kangaskhan”, “Horsea”, “Seadra”, “Goldeen”, “Seaking”, “Staryu”, “Starmie”, “Scyther”, “Jynx”, “Electabuzz”, “Magmar”, “Pinsir”, “Tauros”, “Magikarp”, “Gyarados”, “Lapras”, “Ditto”, “Eevee”, “Vaporeon”, “Jolteon”, “Flareon”, “Porygon”, “Omanyte”, “Omastar”, “Kabutops”, “Aerodactyl”, “Snorlax”, “Articuno”, “Zapdos”, “Moltres”, “Dratini”, “Dragonair”, “Dragonite”, “Mewtwo”, “Mew”
+];
+
+const maxTries = 10;
+
+var guessedLetters = [];
+var currentWordIndex;
+var guessingWord = [];
+var remainingGuesses = 0;
+var gameStarted = false;
+var hasFinished = false;
+var wins = 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{/* <audio controls id="music">
+<source src="mj.mp3" type="audio/mpeg">
+Your browser does not support the audio element.
+</audio>
+
+<p>Click the buttons to play or pause the music.</p>
+
+<button onclick="play()" type="button">Play </button>
+<button onclick="pause()" type="button">Pause</button>
+
+<script>
+var myMusic= document.getElementById("music");
+function play() {
+myMusic.play();
+}
+
+function pause() {
+myMusic.pause();
+}
+</script> */}
+
+// function play() {
+//     var audio = new Audio();
+//     audio.play();
+// }
